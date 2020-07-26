1ead255be8b706b37abf611093c0220230d8a335
Toshio Imaoka <pumpkinpie.tea@gmail.com>
2020-07-26 20:26:43 +0900

    add itertoos

---
+++ python3/itertools.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import itertools
+inputlist = list(map(str, input().split()))
+
+# permutationsをつかうと順列（全パターンの並び順）を表示できる
+outputlist = list(itertools.permutations(inputlist))
+print('# result as it is.')
+print(outputlist)  # 単純な結果表示
+print('--------------')
+print('')
+print('result as words')
+print(','.join([''.join(outputlist[i])
+                for i in range(len(outputlist))]))  # 文字列をつなげて単語として出力
+print('--------------')
+print('')
