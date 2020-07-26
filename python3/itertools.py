aaaf037447e556991a8c4079f4c5a6129d419ebc
Toshio Imaoka <pumpkinpie.tea@gmail.com>
2020-07-26 21:04:25 +0900

    add #

--- python3/itertools.py
+++ python3/itertools.py
@@ -6,10 +6,10 @@ inputlist = list(map(str, input().split()))
 outputlist = list(itertools.permutations(inputlist))
 print('# result as it is.')
 print(outputlist)  # 単純な結果表示
-print('--------------')
+print('# --------------')
 print('')
-print('result as words')
+print('# result as words')
 print(','.join([''.join(outputlist[i])
                 for i in range(len(outputlist))]))  # 文字列をつなげて単語として出力
-print('--------------')
+print('# --------------')
 print('')
